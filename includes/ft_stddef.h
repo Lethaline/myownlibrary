3fc5dff7628c564613439835dc6ebd38201bdb22
lethaline <lethartiflette@proton.me>
2023-12-18 02:04:38 +0100

    [ADD-0] Integration of gcov into the test rule

---
+++ includes/ft_stddef.h
@@ -0,0 +1,5 @@
+#ifndef FT_STDDEF_H
+# define FT_STDDEF_H
+
+typedef long unsigned int size_t;
+#endif
---
+++ tests/ft_string/ft_test_strlen.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "../../includes/ft_stddef.h"
+#include "../../includes/ft_string.h"
+
+int	main()
+{
+	printf("ft_strlen : \n");
+	printf("%lu\n", ft_strlen("Salut"));
+}
